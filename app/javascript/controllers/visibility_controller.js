f5f224ef94fe5625bf768da49df0ad52689e8184
Alexandre Cornuau <cornuau.alexandre1@gmail.com>
2026-02-02 15:17:39 +0100

    improve svg animation

--- app/javascript/controllers/visibility_controller.js
+++ app/javascript/controllers/visibility_controller.js
@@ -3,7 +3,7 @@ import Swal from "sweetalert2";
 
 // Connects to data-controller="visibility"
 export default class extends Controller {
-  static targets = ["image"]
+  static targets = ["image" , "rect"]
 
   static values = {
     notice: String,
@@ -13,7 +13,8 @@ export default class extends Controller {
   connect() {
     this.index = 0
     this.imageTargets[this.index].classList.remove("hidden")
-    this.startTimer();
+    this.startTimer()
+    this.updateSvg();
 
   }
 
@@ -38,6 +39,8 @@ export default class extends Controller {
       clearInterval(this.interval_Exercice);
       this.rotation();
       this.startTimer();
+      this.updateSvg();
+
 
     }
 
@@ -46,11 +49,29 @@ export default class extends Controller {
           background: '#3e3e3e',
           color: '#ffffff',
           showConfirmButton: false,
-          imageHeight: 1500,
           icon: "success",
-          timer: 1500
+          timer: 1500,
     });
   }
+
+  updateSvg() {
+  // récupérer les dimensions de la card
+  const elem =  this.imageTargets[this.index];
+  //récupérer l'element Svg
+  const container = this.rectTargets[this.index];
+  // donner les dimension de la card au svg
+  const rect = elem.getBoundingClientRect();
+  console.log(elem);
+  console.log(rect.height);
+  container.setAttribute("width", rect.width);
+  container.setAttribute("height", rect.height);
+
+  const perimeter = 2 * (rect.width + rect.height);
+  container.style.strokeDasharray = perimeter;
+  container.style.strokeDashoffset = perimeter;
+
+}
+
 
 
 }
