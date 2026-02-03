f6873004a762fb4b2c1c2237f6488a30664b3c7b
Alexandre Cornuau <cornuau.alexandre1@gmail.com>
2026-02-03 21:54:14 +0100

    add final alert

--- app/javascript/controllers/visibility_controller.js
+++ app/javascript/controllers/visibility_controller.js
@@ -1,16 +1,15 @@
 import { Controller } from "@hotwired/stimulus";
 import Swal from "sweetalert2";
 
+
 // Connects to data-controller="visibility"
 export default class extends Controller {
   static targets = ["image" , "rect"]
 
-  static values = {
-    notice: String,
+  // static values = {
+  //   notice: String,
 
-  }
-
-
+  // }
 
 
   connect() {
@@ -30,23 +29,41 @@ export default class extends Controller {
     this.imageTargets[this.index].classList.add("hidden")
     if (this.index < this.imageTargets.length - 1) {
       this.index ++
+      this.imageTargets[this.index].classList.remove("hidden")
+      this.notice()
+      const audio = new Audio('/beep.mp3');
+      audio.play();
+      this.updateSvg();
+    } else {
+      this.imageTargets[this.index].classList.remove("hidden")
+       this.finish()
+
+    }
+
+  }
+
+   reverseRotation() {
+    this.imageTargets[this.index].classList.add("hidden")
+    if (this.index > 0) {
+      this.index -=1
     } else {
       this.index = 0
     }
     this.imageTargets[this.index].classList.remove("hidden")
-    this.notice()
-    const audio = new Audio('/beep.mp3');
-    audio.play();
+    this.updateSvg();
 
   }
+
+   prev() {
+      clearInterval(this.interval_Exercice);
+      this.reverseRotation();
+      this.startTimer();
+    }
 
     next() {
       clearInterval(this.interval_Exercice);
       this.rotation();
       this.startTimer();
-      this.updateSvg();
-
-
     }
 
       notice() {
@@ -56,7 +73,23 @@ export default class extends Controller {
           showConfirmButton: false,
           icon: "success",
           timer: 1500,
-    });
+        });
+      }
+
+      finish() {
+        const successIcon =
+        '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M208.3 64L432.3 64C458.8 64 480.4 85.8 479.4 112.2C479.2 117.5 479 122.8 478.7 128L528.3 128C554.4 128 577.4 149.6 575.4 177.8C567.9 281.5 514.9 338.5 457.4 368.3C441.6 376.5 425.5 382.6 410.2 387.1C390 415.7 369 430.8 352.3 438.9L352.3 512L416.3 512C434 512 448.3 526.3 448.3 544C448.3 561.7 434 576 416.3 576L224.3 576C206.6 576 192.3 561.7 192.3 544C192.3 526.3 206.6 512 224.3 512L288.3 512L288.3 438.9C272.3 431.2 252.4 416.9 233 390.6C214.6 385.8 194.6 378.5 175.1 367.5C121 337.2 72.2 280.1 65.2 177.6C63.3 149.5 86.2 127.9 112.3 127.9L161.9 127.9C161.6 122.7 161.4 117.5 161.2 112.1C160.2 85.6 181.8 63.9 208.3 63.9zM165.5 176L113.1 176C119.3 260.7 158.2 303.1 198.3 325.6C183.9 288.3 172 239.6 165.5 176zM444 320.8C484.5 297 521.1 254.7 527.3 176L475 176C468.8 236.9 457.6 284.2 444 320.8z"/></svg>'
+
+        Swal.fire({
+          title: 'finish!',
+          background: '#3e3e3e',
+          icon: 'success',
+          iconHtml: successIcon,
+          iconColor:'#a2ff30',
+          customClass: {
+            icon: 'rotate-y',
+          },
+    })
   }
 
   updateSvg() {
@@ -69,7 +102,7 @@ export default class extends Controller {
     const r = 35; // rayon augmenté pour compenser l'effet visuel du squircle
 
     // Squircle : la courbe commence plus tôt pour une transition douce
-    // Coefficients augmentés pour plus de smoothing
+
     const p = r * 2;
     const l = r * 0.2;
 
